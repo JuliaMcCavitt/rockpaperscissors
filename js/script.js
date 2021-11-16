5c878d98c514774be5a546627b756d1b852f135f
Glitch (verdant-mirage-hook) <none>
2021-11-16 23:23:52 +0000

    ⛱🌜 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+$(".play").click(function() {
+    let order= $("input").val();
+    $(".userChoice").append("<p>"+order+"</p>");
+});
 
+$(".play").click(function() {
+    let order= Math.random();
+  if (input <0.33)=
+    $(".computerChoice").text("rock");
+   if (0.66> input >0.33)=
+    $(".computerChoice").text("paper");
+ if (input >0.66)=
+    $(".computerChoice").text("scissors");
+});
