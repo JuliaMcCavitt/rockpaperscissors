e9768a4595ce642695c174bdf337cb5f77541bb9
Glitch (verdant-mirage-hook) <none>
2021-12-14 23:19:10 +0000

    🦃🐛 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,37 +1,40 @@
 $(".play").click(function() {
   let order = $("input").val();
   $(".userChoice").append("<p>" + order + "</p>");
-});
-
-$(".play").click(function() {
   let list = Math.random();
   console.log(list);
   if (list < 0.33) $(".computerChoice").append("<p>" + "rock" + "</p>");
   else if (list < 0.66) $(".computerChoice").append("<p>" + "paper" + "</p>");
-  else if (list > 0.66)
-    $(".computerChoice").append("<p>" + "scissors" + "</p>");
+  else if (list > 0.66) $(".computerChoice").append("<p>" + "scissors" + "</p>");
+  
 
-  let order = $("input").val();
-  let list = Math.random();
+  if ( order.toLowerCase() === "rock" ()&& list < 0.66) {$(".result").text("Computer Wins!");}
+  
+  else if ( order.toLowerCase() === "paper" && list < 0.66) {$(".result").text("Tie");}
+  
+  else if ( order.toLowerCase() === "scissors" && list < 0.66) {$(".result").text("Player Wins!");}
+  
+  else if ( order.toLowerCase() === "rock"&& list < 0.33) {$(".result").text("Tie");}
 
-  if (order === "rock"&& list < 0.66) 
-  {$(".result").text("user picked rock")}
-  else if (order === "paper") {$(".result").text("user picked paper");
-  } else if (list < 0.66) $(".result").text("Tie");
-  else if (order === "scissors") {
-  } else if (list < 0.66) $(".result").text("Player Wins!");
-  else if (order === "rock") {
-  } else if (list < 0.33) $(".result").text("Tie");
-  else if (order === "paper") {
-  } else if (list < 0.33) $(".result").text("Player Wins!");
-  else if (order === "scissors") {
-  } else if (list < 0.33) $(".result").text("Computer Wins!");
-  else if (order === "rock") {
-  } else if (list > 0.66) $(".result").text("Player Wins!");
-  else if (order === "paper") {
-  } else if (list > 0.66) $(".result").text("Computer Wins!");
-  else if (order === "scissors") {
-  } else if (list > 0.66) $(".result").text("Computer Wins!");
+  
+  else if ( order.toLowerCase()=== "paper"&& list < 0.33) {$(".result").text("Player Wins!");}
+   
+  
+  else if ( order.toLowerCase()=== "scissors" && list < 0.33) {$(".result").text("Computer Wins!");}
+   
+  
+  else if ( order.toLowerCase()=== "rock" && list > 0.66) {$(".result").text("Player Wins!");}
+ 
+  
+  else if ( order.toLowerCase()=== "paper"&& list > 0.66) {$(".result").text("Computer Wins!");}
+
+  
+  else if( order.toLowerCase()=== "scissors" && list > 0.66) {$(".result").text("Tie!");}
+   
+  
+  else(
+ $(".result").text("Please re-enter your choice")
+)
 });
 
 //if (userChoice === "paper" && computerChoice === "rock") {
